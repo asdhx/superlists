ca96838ccbab37f9f0be7c301ab6b6263bbce127
max <max@gmail.com>
2020-02-26 13:17:25 +0100

    ?

--- lists/tests.py
+++ lists/tests.py
@@ -19,6 +19,11 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
 
@@ -26,5 +31,3 @@ class HomePageTest(TestCase):
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
-
-        self.assertTemplateUsed(response, 'wrong.html')
