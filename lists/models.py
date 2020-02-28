87b279703a1e504e69c22e38812c3566500de3d5
max <max@gmail.com>
2020-02-28 14:14:33 +0100

    idk if that's right but every test after ch7 works

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
+class List(models.Model):
+    name = models.CharField(max_length=100)
+
+
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, on_delete=models.CASCADE, default=None)
