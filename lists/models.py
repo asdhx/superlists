22b2c01d62c260ee5b39051eab6f304356d28320
max <max@gmail.com>
2020-03-18 13:03:09 +0100

    Use get_absolute_url on List model to DRY urls in views

--- lists/models.py
+++ lists/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.shortcuts import reverse
 
 class List(models.Model):
     name = models.CharField(max_length=100)
 
-
+    def get_absolute_url(self):
+        return reverse('view_list', args=[self.id])
 
 class Item(models.Model):
     text = models.TextField(default='')
