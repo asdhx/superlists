ef89ea5605cd95eadabb3890de21bb3c7c5b983d
max <max@gmail.com>
2020-03-21 12:21:20 +0100

    so

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -7,7 +7,7 @@ from django.utils.html import escape
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 from lists.models import Item, List
 from lists.views import home_page
-
+from unittest import skip
 
 class HomePageTest(TestCase):
 
@@ -139,3 +139,13 @@ class NewListTest(TestCase):
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.client.post('/lists/new', data={'text': ''})
         self.assertIsInstance(response.context['form'], ItemForm)
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(f'/lists/{list1.id}/', data={'text': 'textey'})
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
